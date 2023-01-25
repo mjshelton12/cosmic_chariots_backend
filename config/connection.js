a0fbba99aaa1755f0904067d0de795e580028e57
MJ_Shelton <mjshelton12@comcast.net>
2023-01-24 20:10:13 -0600

    Redeploy

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://mjshelton12:Password123@cosmic-cluster.wnatxoj.mongodb.net/Cosmic-Chariots?retryWrites=true&w=majority',
-  
+  process.env.MONGODB_URI || 'mongodb+srv://mjshelton12:Password123@cosmic-cluster.wnatxoj.mongodb.net/?retryWrites=true&w=majority',
+
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
