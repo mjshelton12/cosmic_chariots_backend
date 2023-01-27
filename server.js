969c27fa71408652b7acbfe949be803a15e601f6
MJ_Shelton <mjshelton12@comcast.net>
2023-01-26 22:42:37 -0600

    Redeploy

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth')
+// const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+//   context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '..app/client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '..app/client/build/index.html'));
 });
 
+// app.get('/', function (req, res) {
+//   const index = path.join(__dirname, 'build', 'index.html');
+//   res.sendFile(index);
+// });
 
-// Create a new instance of an Apollo server with the GraphQL schema
+// New instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -39,5 +43,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
   
-// Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
