29b897b3451c6567ac65e7dbadc58b859bbd5f07
MJ_Shelton <mjshelton12@comcast.net>
2023-01-28 16:53:49 -0800

    Redeploy

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..app/client/build')));
-}
+app.use(express.static(path.join(__dirname, '..app/client/build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..app/client/build/index.html'));
